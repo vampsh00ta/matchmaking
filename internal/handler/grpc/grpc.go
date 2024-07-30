974ac77a078f9f78686e26149fccaa4e0515a1e2
vampsh00ta <trofimov.vlad-1234@yandex.ru>
2024-07-30 13:47:16 +0300

    added logger processing

--- internal/handler/grpc/grpc.go
+++ internal/handler/grpc/grpc.go
@@ -25,25 +25,37 @@ func Register(s *grpc.Server, matchmaking *serverAPI) {
 }
 
 func (s *serverAPI) FindMatch(ctx context.Context, in *pb.FindMatchRequest) (*pb.FindMatchResponse, error) {
+	methodName := "FindMatch"
 	if in.TgID == 0 {
+
+		s.l.Error(methodName, status.Error(codes.InvalidArgument, "tg_id is required"))
 		return nil, status.Error(codes.InvalidArgument, "tg_id is required")
 	}
 
 	foundTgID, err := s.matchmaking.FindMatch(ctx, int(in.TgID))
 	if err != nil {
+		s.l.Error(methodName, err)
+
 		return nil, status.Error(codes.Internal, err.Error())
 	}
+	s.l.Info(methodName, zap.String("status", "ok"))
 
 	return &pb.FindMatchResponse{TgID: int64(foundTgID)}, nil
 }
 func (s *serverAPI) MatchResult(ctx context.Context, in *pb.MatchResultRequest) (*pb.MatchResultResponse, error) {
+	methodName := "MatchResult"
+
 	if in.TgIDLoser == 0 || in.TgIDWinner == 0 {
+		s.l.Error(methodName, status.Error(codes.InvalidArgument, "tg_id is required"))
+
 		return nil, status.Error(codes.InvalidArgument, "tg_id is required")
 	}
 
 	if err := s.matchmaking.MatchResult(ctx, int(in.TgIDWinner), int(in.TgIDLoser)); err != nil {
+		s.l.Error(methodName, err)
 		return nil, status.Error(codes.Internal, err.Error())
 	}
+	s.l.Info(methodName, zap.String("status", "ok"))
 
 	return &pb.MatchResultResponse{Ok: true}, nil
 }
